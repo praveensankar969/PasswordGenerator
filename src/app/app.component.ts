e324342b697aeb346c49010ae9195d193cad8d92
Praveen <sankar_praveen@hotmail.com>
2021-06-24 14:41:07 +0530

    link added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,65 +7,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Dev';
-  password :string ="";
-  length : number = 0;
-  letter :boolean = false;
-  number : boolean= false;
-  symbol :boolean = false;
+  password: string = "";
+  length: number = 0;
+  letter: boolean = false;
+  number: boolean = false;
+  symbol: boolean = false;
 
-  Generate(){
-    const numbers= '1234567890';
+  Generate() {
+    const numbers = '1234567890';
     const letters = "abcdefghijklmnopqrstuvwxyz";
     const symbols = "!@#$%^&*()";
-    var validChar ="";
-    if(this.letter){
+    var validChar = "";
+    if (this.letter) {
       validChar += letters;
     }
-
-    if(this.number){
+    if (this.number) {
       validChar += numbers;
     }
-
-    if(this.symbol){
+    if (this.symbol) {
       validChar += symbols;
     }
-
     var password = "";
-    for(var i=0;i<this.length;i++){
+    for (var i = 0; i < this.length; i++) {
       const index = Math.floor(Math.random() * validChar.length);
       password += validChar[index];
     }
-
     this.password = password;
-
-
-
     console.log(this.password)
   }
 
-  LetterSelected(){
-    this.letter = ! this.letter;
+  LetterSelected() {
+    this.letter = !this.letter;
   }
 
-  NumberSelected(){
+  NumberSelected() {
     this.number = !this.number;
   }
 
-  SymbolSelected(){
+  SymbolSelected() {
     this.symbol = !this.symbol;
   }
-  OnChangeLength(length : Event){
+  OnChangeLength(length: Event) {
     const value = parseInt((length.target as HTMLInputElement).value);
-    if(length.target){
+    if (length.target) {
       this.length = 0;
-      this.password="";
+      this.password = "";
     }
-    if(!isNaN(value)){
+    if (!isNaN(value)) {
       this.length = value;
     }
-    
-   
   }
-
-
 }
